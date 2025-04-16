74d82a31ce2eb8a5e9e7a8f27e3e8fb4df76bf6b
samanthacuenot <s.cuenot@ufl.edu>
2025-04-15 21:04:54 -0400

    Layout for coding

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import pandas as pd
 
 df = pd.read_csv('songs_normalize.csv')
-print(df.head(50))
+
+'''
+First going to make a baseline for music popularity from 2000 to 2019. In this, we are specifically looking at 
+    (1) most popular genre each year
+    (2) most popular artist each year
+'''
+
+
+
+
+'''
+Then, within the most popular genre and artist, we are going to look at their music characteristics, including
+    - Danceability
+    - Tempo
+    - Average duration of their music (for artist specifically)
+'''

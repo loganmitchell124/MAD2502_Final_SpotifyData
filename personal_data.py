46b1c4f8ebece1d33bbcc5396d74e58cc37e6b61
samanthacuenot <s.cuenot@ufl.edu>
2025-04-23 20:28:19 -0400

    Upload personal spotify data code

--- personal_data.py
+++ personal_data.py
@@ -1,4 +1,7 @@
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def import_json_from_user():
     """
@@ -20,3 +23,36 @@ def import_json_from_user():
         print("Error: " + str(e))
         return None
 
+
+def plot_top_artists(data, top_n = 10):
+    """
+    Author: Samantha Cuenot
+    Summary: takes user spotify data and plots top n artists based on the ms streamed
+    """
+    artist_listen_time = {}
+
+    for entry in data:
+        artist = entry['artistName']
+        ms_played = entry['msPlayed']
+        if artist in artist_listen_time:
+            artist_listen_time[artist] += ms_played
+        else:
+            artist_listen_time[artist] = ms_played
+
+    # Convert the ms to hours
+    artist_hours = {artist: ms / (1000 * 60 * 60) for artist, ms in artist_listen_time.items()}
+    artist_df = pd.DataFrame(list(artist_hours.items()), columns=['Artist', 'HoursListened'])
+    artist_df.sort_values('HoursListened', ascending=False, inplace=True)
+
+    top_artists = artist_df.head(top_n)
+    plt.figure(figsize=(10, 10))
+    plt.barh(top_artists['Artist'], top_artists['HoursListened'])
+    plt.xlabel('Hours Listened')
+    plt.ylabel(f'Top {top_n} Listened to Artists by You')
+    plt.gca().invert_yaxis()
+    plt.tight_layout()
+    plt.show()
+
+spotify_data = import_json_from_user()
+if spotify_data:
+    plot_top_artists(spotify_data)

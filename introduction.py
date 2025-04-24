8c561eacbf9ea1240dc0a38c21d5e133ddc52e1b
samanthacuenot <s.cuenot@ufl.edu>
2025-04-23 20:10:48 -0400

    Explanation/layout for user interface

---
+++ introduction.py
@@ -0,0 +1,16 @@
+"""
+User interface:
+First, provide introduction to what the project is, who it's by, etc.
+
+After introduction screen, go to main display menu, where user can select between two options
+    1. Upload their own personal spotify data (JSON file)
+    2. Explore trends from Billboard's 2000-2019 data
+
+If user selects option 1, automatically generate graphs that shows their most popular artist by amount of time listened to or something.
+
+If user selects option 2, they have 4 options:
+    1. Take a look by year (what logan already did with the artist and genre by year)
+    2. Show popularity over time of genres
+    3. Explore their favorite artist (the idea is for them to input their own artist, see if its in the datafile, and look at their stats)
+    4. Explore relationships between variables (the function min already did)
+"""
